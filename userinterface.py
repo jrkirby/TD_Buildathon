f39a5cbc51d35bddd6d3d2622c6e34e494e89b5e
Austin Walters <lettergramm@gmail.com>
2013-11-16 21:55:09 -0600

    Update userinterface.py

--- userinterface.py
+++ userinterface.py
@@ -33,15 +33,20 @@ The number of pixels between two lines of text on the screen.
 FONT_LINESPACE = 4
 
 import pygame
+import hand
 
 class UserInterface:
 
-    def __init__(self, hand):
+    def __init__(self):
         # Define a font object to use
         pygame.font.init()
         self.font = pygame.font.Font(os.path.join("UI", "larabie.ttf"), FONT_SIZE, )
-        self.gamestate = True # The game is running
-	self.hand = hand
+        self.gamestate = False # The game is running
+
+    def start(self, deck, surface):
+        self.gamestate = True
+        self.hand = hand.Hand(deck, surface)
+        self.card_selected = False
 
     def update(self, gamedata):
         # We save a surface containing the text we want to show.
@@ -53,19 +58,43 @@ class UserInterface:
                                           True, FONT_COLOR, FONT_BACKGROUND)
         self.defeat = self.font.render("You have been defeated!", True,
                                        FONT_COLOR, FONT_BACKGROUND)
+
+        if(self.card_selected):
+            self.current_mouse_pos = pygame.mouse.get_pos()
+            new_position = (self.hand.cards[self.clicked_on].position[0] + self.current_mouse_pos[0] - self.prev_mouse_pos[0], self.hand.cards[self.clicked_on].position[1] + self.current_mouse_pos[1] - self.prev_mouse_pos[1])
+            self.hand.cards[self.clicked_on].position = new_position
+            self.prev_mouse_pos = self.current_mouse_pos
+    
+    def click(self, x, y):
+        self.clicked_on = self.hand.check(x, y)
+        if(self.clicked_on == -1):
+            return
+        self.card_selected = True
+        self.prev_mouse_pos = (x, y)
+
+    def click_up(self, surface, map):
+        #print("clickup")
+        if(self.card_selected):
+            self.card_selected = False
+            self.hand.cards[self.clicked_on].position = self.hand.index_to_pos(self.clicked_on, surface)
+            if(map.checkIfCanPlace(self.prev_mouse_pos[0], self.prev_mouse_pos[1])):
+                t = map.placeTowerAt(self.prev_mouse_pos[0], self.prev_mouse_pos[1], self.hand.cards[self.clicked_on].ctype)
+                pygame.display.update()
+		return t
+	return -1
+
 	
 
     def draw(self, surface):
         width = surface.get_width()
         height = surface.get_height()
         # Draws the hand below the bottom of the map
-	x = 0
-        for card in self.hand.cards :            
-            y = height - 133
-            card.position_x = x
-            card.position_y = y
-            card.draw(surface)
-            x += 100
+        
+        # for card in self.hand.cards :
+        #     surface.blit(self.basecard, card.position.x, card.position.y)
+
+        self.hand.draw(surface)
+
         # Draw the score in the upper left corner
         surface.blit(self.score, (FONT_PADDING, FONT_PADDING))
         # Put the number of lives below the score
